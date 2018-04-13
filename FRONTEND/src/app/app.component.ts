bdfb6a1a06dc402f8baa3aa3d2c1bbb975207a56
Chris <mosser4567@gmail.com>
2018-04-13 15:09:14 -0400

    base project setup using VSCode IDE

---
+++ FRONTEND/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'plunge-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title: string = "The Plunge";
+}
---
+++ FRONTEND/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plunge-dashboard',
+  templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
