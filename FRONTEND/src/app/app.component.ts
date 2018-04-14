9f4dc60ba4c2e637ccbc76abb24e7a80a0ce43e0
Chris <mosser4567@gmail.com>
2018-04-14 14:07:40 -0400

    implement basic route guard in angular

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { AppUserAuth } from './security/app-user-auth';
+import {SecurityService} from './security/security.service';
 
 @Component({
   selector: 'plunge-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  securityObject: AppUserAuth = null;
+  
   title: string = "The Plunge";
+
+  constructor(private securityService: SecurityService) {
+    this.securityObject = this.securityService.securityObject;
+  }
+
+  logout() : void {
+    this.securityService.logout();
+  }
 }
